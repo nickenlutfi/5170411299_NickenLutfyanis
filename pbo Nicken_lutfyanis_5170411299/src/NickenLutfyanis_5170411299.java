b5dcbde6cd8e7695d969b0aae339e8f7f8a5f37b
nickenlutfi <44410710+nickenlutfi@users.noreply.github.com>
2018-10-24 02:15:35 +0700

    Add files via upload

---
+++ pbo Nicken_lutfyanis_5170411299/src/NickenLutfyanis_5170411299.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class NickenLutfyanis_5170411299 {
+  public static void main(String[]args){
+
+int change;
+Scanner s = new Scanner (System.in);
+
+for (change=1; change<=3;change++)
+{
+    System.out.println("1. Input data ");
+    System.out.println("2. Tampil data ");
+    System.out.println("3. Keluar ");
+    System.out.print("Pilihan: "); change = s.nextInt();
+}
+        
+}  
+}
+
